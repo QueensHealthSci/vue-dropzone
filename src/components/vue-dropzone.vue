307a3acff519b561586a2f6a04dc4033fce45dfc
Grace <glyl@queensu.ca>
2020-07-07 12:27:36 -0400

    always show dropzone-previews container

--- src/components/vue-dropzone.vue
+++ src/components/vue-dropzone.vue
@@ -1,7 +1,6 @@
 <template>
   <div :id="id" ref="dropzoneElement" :class="{ 'vue-dropzone dropzone': includeStyling }">
-    <div v-if="customPreviewsContainer"
-         class="dropzone-previews"
+    <div class="dropzone-previews"
     />
     <!-- displayUploadMessage is in a v-show since dropzone generates its own default message if
     it does not detect the presence of a '.dz-message' element when the dropzone element is created -->
